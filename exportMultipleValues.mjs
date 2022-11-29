06687d55f1e311452ee40cfe1c112e41f5d8e31c
sandy <velpulasandeep321@gmail.com>
2022-11-29 03:04:37 +0000

    I am getting error

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,5 @@
+export let myArray = ["camel", 265, true, "5.6"];
+export let buld = { watts: 10, type: "LED" };
+export function multiplyByFour(num) {
+  return num * 4;
+}
---
+++ importMultipleValues.mjs
@@ -0,0 +1,5 @@
+import { myArray, buld, multiplyByFour } from "./exportMultipleValues.mjs";
+
+console.log(myArray);
+console.log(buld);
+console.log(multiplyByFour(5));
